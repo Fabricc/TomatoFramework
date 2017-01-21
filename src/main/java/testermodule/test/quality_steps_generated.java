063ac713ad363741d946369468397f7970b01ab8
Fabio <Fabio@192.168.1.100>
2017-01-21 12:11:10 +0100

    updatate todo

--- src/main/java/testermodule/test/quality_steps_generated.java
+++ src/main/java/testermodule/test/quality_steps_generated.java
@@ -230,6 +230,22 @@ public class quality_steps_generated {
 		
 	}
 	
+	@Given("^the price of a \"(.*?)\" is (\\d+)c$")
+	public void the_price_of_a_is_c(String arg1, int arg2) throws Throwable {
+	// Write code here that turns the phrase above into concrete actions
+	throw new PendingException();
+	}
+	@When("^I checkout (\\d+) \"(.*?)\"$")
+	public void i_checkout(int arg1, String arg2) throws Throwable {
+	// Write code here that turns the phrase above into concrete actions
+	throw new PendingException();
+	}
+	@Then("^the total price should be (\\d+)c$")
+	public void the_total_price_should_be_c(int arg1) throws Throwable {
+	// Write code here that turns the phrase above into concrete actions
+	throw new PendingException();
+	}
+	
 	
 
 	}
